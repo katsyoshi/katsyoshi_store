f0d64a94518fd9b161854785f196bd4a59e9da4b
MATSUMOTO Katsuyoshi <matsumoto.katsuyoshi+github@gmail.com>
2013-03-12 16:42:15 +0900

    Update toshi_a_store.rb

--- toshi_a_store.rb
+++ toshi_a_store.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-Plugin.create :toshi_a_store do
+Plugin.create :katsyoshi_store do
   filter_mikustore_plugins do |plugins|
     Dir[File.expand_path(File.join(File.dirname(__FILE__), "plugins", "*"))].each{ |package_file|
       plugins << YAML.load_file(package_file).symbolize
